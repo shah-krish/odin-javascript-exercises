64a8cfc42e6e5383a23850f0536abdb8e9df4732
shah-krish <krishshah1411@gmail.com>
2025-05-06 23:00:58 -0400

    Finish 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, arg) {
+    for(let i =0; i<arr.length; i++){
+        if(arr[i] == arg){
+            arr.splice(i,1);
+        }
+       
+    }
+    return arr;
 };
 
 // Do not edit below this line
