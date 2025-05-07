fa11b0477a27e43a998b55e19c2c1098e30ab86f
shah-krish <krishshah1411@gmail.com>
2025-05-06 23:11:39 -0400

    Finish 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(num1<num2){
+        let answer = 0;
+        for(let i = num1; i<=num2; i++){
+            answer+=i;
+        }
+        return answer;
+    }
 };
 
 // Do not edit below this line
