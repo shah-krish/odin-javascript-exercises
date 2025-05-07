2ca1bc98648043d5a98a1dc873c2e1f3ec6621e9
shah-krish <krishshah1411@gmail.com>
2025-05-06 23:20:48 -0400

    Finish 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let ans = 0; 
+  ans = (temp-32) * 5/9;
+  return Math.round(ans * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let ans = 0; 
+  ans = (temp*9)/5 + 32;
+  return Math.round(ans * 10) / 10;
 };
 
 // Do not edit below this line
