8ffa78d50d5de6fe98c51dc6c388e43c5dce5d1e
shah-krish <krishshah1411@gmail.com>
2025-05-09 19:20:45 -0400

    Finish 12_calculator and 13_palindromes. Fix bugs in 07_reverseString, 08_removeFromArray

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let ans = 0;
+  for(let i = 0; i<arr.length; i++){
+    ans=arr[i]+ans;
+  }
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let ans = 1;
+  for(let i = 0; i<arr.length; i++){
+    ans=arr[i]*ans;
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let ans = 1;
+	for(let i = 1; i<=exp; i++){
+    ans = base * ans;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num==0){
+    return 1;
+  }
+  else if(num == 1){
+    return 1;
+  }
+  let ans = 1;
+  while(num>0){
+    ans = ans*num;
+    num--;
+  }
+  return ans;
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let low = 0;
+    let high = str.length-1;
+    while(low<high){
+        if(str[low] == " "){
+            low++;
+        }
+        else if(str[high] == " "){
+            high --;
+        }
+        else if(str[low]!=str[high]){
+            return false;
+        }
+        else{
+            low++;
+            high --;
+        }
+       
+    }
+    return true;
 };
 
 // Do not edit below this line
